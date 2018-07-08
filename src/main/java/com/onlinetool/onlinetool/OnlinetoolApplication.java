42ff38666e85330c61fe3c3c4ef07e96fe398884
Vinoth1989 <Vinoth.vinoj@gmail.com>
2018-07-08 00:30:20 -0500

    intial commit

---
+++ src/main/java/com/onlinetool/onlinetool/OnlinetoolApplication.java
@@ -0,0 +1,12 @@
+package com.onlinetool.onlinetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetoolApplication.class, args);
+	}
+}
